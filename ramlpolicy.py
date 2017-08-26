67ddfa51aa8c7f71bb488103f92506cb5330e6fa
Alan Crosswell <alan@columbia.edu>
2017-08-26 12:16:35 -0400

    externalize RAML policy compiler

--- ramlpolicy.py
+++ ramlpolicy.py
@@ -1,63 +1,117 @@
+#!/usr/bin/env python
+"""
+ramlpolicy: Parse an API's RAML and generate a compact table for inclusion in the
+API Manager custom OAuth 2.0 scope enforcement policy.
+"""
 import json
 import re
 import yaml
 from pprint import pprint
+import logging
+
+log = logging.getLogger()
+if not log.handlers:
+  handler = logging.StreamHandler()
+  formatter = logging.Formatter('%(levelname)-5s %(asctime)s %(message)s')
+  handler.setFormatter(formatter)
+  log.addHandler(handler)
+log.setLevel(logging.DEBUG)
 
 httpVerbs = ['get','post','put','update','patch','delete']
 
-oauth2Scopes = {} # default scopes for the given scheme name if not overidden in a securedBy
+# table of scopes defined in the securitySchemes
+# N.B. MuleSoft APIkit currently has a bug that requries all possible scopes to be enumerated
+# This is just plain wrong.
+oauth2Scopes = {}
+
 def securitySchemes(schemes):
+  """
+  Parse the securitySchemes to collect the OAuth 2.0 scheme names and scopes
+  """
   for s in schemes:
-    if 'type' in schemes[s] and schemes[s]['type'].lower() == 'oauth 2.0':
-      oauth2Scopes[s] = schemes[s]['scopes'] if 'scopes' in schemes[s] else []
+    if 'type' in schemes[s]:
+      if schemes[s]['type'].lower() == 'oauth 2.0':
+        oauth2Scopes[s] = schemes[s]['scopes'] if 'scopes' in schemes[s] else []
+      else:
+        log.warn('securityScheme %s type %s not handled.'%(s,schemes[s]['type']))
   return
 
 def securedBy(schemes): # the scheme name can be a string or a map key
+  """
+  Parse the securedBy list for the named OAuth 2.0 securitySchemes and use the default
+  scopes or override them.
+  """
   sl = []
+  if not isinstance(schemes,list):
+    log.warn('RAML parse error: securedBy expects a list')
+    return sl
   for s in schemes:
     if isinstance(s,str): # if the node is terminal then no overriding scopes
       if s in oauth2Scopes:
-        sl = sl + oauth2Scopes[s] # use the defaults for this scheme name
+        sl.append(oauth2Scopes[s]) # use the defaults for this scheme name
     else: # there _might_ be an override
       for k in s: # should be a dict with one key.
         if 'scopes' in s[k]:
-          sl = sl + s[k]['scopes']
+          sl.append(s[k]['scopes'])
         else:
           if k in oauth2Scopes:
-            sl = sl + oauth2Scopes[s] 
-    return sl
+            sl.append(oauth2Scopes[s])
+  return sl
 
-# make this look like the same keyvalue mustacheMap
+rootScopes = []
+
+def jsonParseRoot(j):
+  """
+  Parse root of document for securitySchemes definition and root securedBy
+  """
+  global rootScopes
+  # document root securitySchemes definition
+  if 'securitySchemes' in j:
+    securitySchemes(j['securitySchemes'])
+  # document root securedBy can set defaults for all methods that don't override with their own securedBy
+  
+  rootScopes = securedBy(j['securedBy']) if 'securedBy' in j else []
+
 def jsonParseResource(j,resource,mustacheMap):
+  """
+  Recursively descend the JSON RAML API document looking for resources (e.g. /things), their
+  associated methods (get, put, etc.) and optional securedBy. Also looks for securitySchemes
+  to identity the names that are referenced in the securedBy and default scope values.
+
+  URL parameters (e.g. /things/{id}) are rewritten as regular expressions (/things/.+).
+  """
   #print(">>>>>>>>>>>>>>ENTER")
   #pprint(j)
+
+  # iterate over keys at this level looking for a /resource and then below that method and under method securedBy
   for m in j:
     if isinstance(m,str): # ignore lists?
       if m[0] == '/':
         #print("<<<<<<<<<<<<<<<<<RECURSE %s"%resource+m)
         #pprint(j[m])
         mustacheMap = jsonParseResource(j[m],resource+m,mustacheMap)
-      elif m.lower() in httpVerbs:
+      elif m in httpVerbs:
         method = m
-        for mj in j[m]:
-          if isinstance(mj,str) and mj.lower() == 'securedby':
-            mr = method + ':' + re.sub('{.+}','.+',resource)
-            #print("adding",{mr: j[m][mj]})
-            mustacheMap.update({mr.replace('/','&#x2F;'): ' '.join(securedBy(j[m][mj]))})
-      elif m.lower() == 'securityschemes':
-        securitySchemes(j[m])
+        mkey = method + ':' + re.sub('{.+}','.+',resource)
+        if 'securedBy' in j[m]:
+          mustacheMap.update({mkey: securedBy(j[m]['securedBy'])})
+        else:
+          mustacheMap.update({mkey: rootScopes})
       else: 
-        pass
-
+        pass # ignore irrelevant keys
+    else:
+      log.debug('ignoring type %s'%type(m))
   return mustacheMap
 
 def ramlParse(s):
   mustacheMap = {}
   y = yaml.load(s.replace('!','')) # temporarily just ignore the !includes and so on
   #pprint(y)
+  jsonParseRoot(y)
   return jsonParseResource(y,'',mustacheMap)
 
 
+### pull in from a URL
 ramlString = """
 #%RAML 1.0  
 title: demo-echo
@@ -72,6 +126,8 @@ documentation:
 #baseUri: https://columbia-demo-echo.cloudhub.io/{version}
 baseUri: https://mocksvc.mulesoft.com/mocks/85d52e43-4825-42f8-a2a6-fcd2ad9d2aaa/{version}
 securitySchemes: 
+  oauth_1_0:
+    type: OAuth 1.0
   oauth_2_0:
     type: OAuth 2.0
     description: |
@@ -114,12 +170,38 @@ securitySchemes:
       - auth-windowslive
 types:
   Thing: !include thingType.raml
+# default top-level securedBy.
+# Due to an APIkit bug that requires enumerating all possible scopes, you need to always
+# override the scopes or end up with an impossible scope list.
+securedBy:
+  - oauth_2_0: { scopes: [] }
+  - oauth_2_0: { scopes: [ root-default, auth-facebook, openid, read ] }
+# above securedBy is use for /stuff get
+/stuff:
+  displayName: Stuff
+  description: some stuff
+  get:
+    responses:
+      200:
+        body:
+          application/json:
+            schema: Stuff
+  /more:
+    displayName: moretuff
+    description: some more stuff
+    post:
+      responses:
+        200:
+          body:
+            application/json:
+              schema: Stuff
 /things:
   displayName: Things
   description: some things
   get:
     securedBy: # allow selection of one of columbia, facebook, or google login. 
       - oauth_2_0: { scopes: [ auth-columbia, openid, read ] }
+      - oauth_2_0: { scopes: [ auth-facebook, openid, read ] }
       - oauth_2_0: { scopes: [ auth-google, openid, read ] }
     queryParameters:
      filter:
@@ -180,5 +262,4 @@ types:
 """
 
 m = ramlParse(ramlString)
-for k,v in m.items():
-  print('"%s": "%s",'%(k.replace('&#x2F;','/'),v))
+print(json.dumps(m))
