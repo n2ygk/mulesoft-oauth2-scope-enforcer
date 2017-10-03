e4a117b6795a41612a95e232f92280cc179c290a
Alan Crosswell <alan@columbia.edu>
2017-10-02 20:56:52 -0400

    initial ramlpolicy.js

---
+++ ramlpolicy.js
@@ -0,0 +1,62 @@
+// ramlpolicy.js: Parse a RAML 1.0 API definition and produce the JSON
+// serialized object for use in OAuth 2.0 scope enforcement policy.
+const {RamlJsonGenerator} = require('raml-json-enhance-node'); // https://github.com/mulesoft-labs/raml-json-enhance-node
+// recursively descend the resource tree, building up the method & resource conditons as in this example:
+//{
+//  "post:/things": [["auth-columbia", "demo-netphone-admin", "create"],
+//                   ["auth-google", "create"],
+//                   ["auth-facebook", "create"]],
+//  "put:/things/.+": [["auth-columbia", "update"]],
+//  "get:/things": [["auth-columbia", "read"],
+//                  ["auth-google", "read"],
+//                  ["auth-facebook", "read"]],
+//  "get:/things/.+": [["read"]],
+//  "get:/things/.+/foo": []
+//}
+var scopeMap = {};
+function Resource(resource) {
+    var resPath = resource['parentUrl']+resource['relativeUri'];
+    var methods = resource['methods']
+    for (var m in methods) {
+	var method = methods[m]['method']
+	var securedBy = methods[m]['securedBy'];
+	var re = /\{.*\}/g;
+	var key = method + ':' + resPath.replace(re, '.+');
+	scopeMap[key] = [];
+	for (var s in securedBy) {
+	    var scopes = securedBy[s]['settings']['scopes'];
+	    var type = securedBy[s]['type'];
+	    if (type == 'OAuth 2.0') {
+		scopeMap[key].push(scopes);
+	    }
+	}
+    }
+    children = resource['resources'];
+    for (var i in children) {
+	Resource(children[i]);
+    }
+}
+// start at the root of API's JSON document and invoke Resource() for each top-level resource.
+function JsonGenerate(rootJson) {
+    //console.log(rootJson['resources'][0]['methods'][0]['securedBy'][0]['settings']['scopes'])
+
+    for (var i in rootJson['resources']) {
+	resource = rootJson['resources'][i];
+	Resource(resource);
+    }
+
+    console.log(JSON.stringify(scopeMap));
+}
+
+// Compile the RAML 1.0 file(s) into a JSON representation.
+function RamlParse(raml) { 
+    const generator = new RamlJsonGenerator(raml, {prettyPrint: false, verbose: false});
+    promise = generator.generate()
+    promise.then((json) => { JsonGenerate(json) });
+}
+
+RamlParse('t/api.raml');
+// this fails, likely due to the self-signed cert.
+//RamlParse('https://localhost:8082/v1/console/api/?raml');
+
+
